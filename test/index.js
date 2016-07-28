95af5250af7e703f2fbac5c33136d62df23757f3
1111hui <26634873@qq.com>
2016-07-29 07:15:32 +0800

    ADD: displayCSS function to display current css

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 
 var css = {
+  textarea:{width:'500px', height:'300px'},
   '#menu1':{
     fontFamily: 'Arial,Helvetica',
     fontSize: '16px',
@@ -26,23 +27,33 @@ var css = {
   }
 }
 
+function cssobj_plugin_post_csstext(callback) {
+  var cb = function(str) {
+    typeof callback=='function' && callback(str)
+  }
+
+}
 
 var com = {
 
   controller : function(arg){
     var self = this
-    // a normal invoke to cssobj
-    var result = cssobj(css, {local:true})
+
+    // invoke cssobj
+    var result = cssobj(css, {local:true, onUpdate: displayCSS})
+
     // get mc instance from result and m
     self.mc = cssobj_mithril(result, m)
+
+
     // mithril model part
-    var nameArr = ['username', 'James Yang', 'Jason Zhou']
     var name = 0
+    var nameArr = ['username', 'James Yang', 'Jason Zhou']
+    self.age = 16
     self.getName = function(next) {
       if(next) name++
-      return nameArr[name % 3]
+      return nameArr[name % nameArr.length]
     }
-    self.age = 16
     self.updateAge = function() {
       self.age++
       // fontSize will be same as age
@@ -72,3 +83,25 @@ var com = {
 }
 
 m.mount(document.getElementById('menu1'), com)
+
+
+// hellper function to display CSSOM text
+
+function displayCSS(result) {
+  var cb = function(str) {
+    document.getElementById('csstext').value = str
+  }
+  var dom = result.cssdom
+
+  if(!dom) return cb('')
+  var sheet = dom.sheet || dom.styleSheet
+  if(sheet.cssText) return cb(sheet.cssText)
+
+  var str = ''
+  var rules = sheet.cssRules || sheet.rules
+  for(var i = 1, len = rules.length; i < len; i++) {
+    str += rules[i].cssText + '\n'
+  }
+
+  return cb(str)
+}
